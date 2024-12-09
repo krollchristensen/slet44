6d6a47dcfe2b614f8bfa78e20024b2787af95cb5
krollchristensen <krollchristensen@gmail.com>
2024-12-09 12:28:58 +0100

    tests

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello   wo   rld!");
     }
 }
